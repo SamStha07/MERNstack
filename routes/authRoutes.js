d36f964248af8e71e3bfc59fe909a4a673035bf4
Sam <samshrestha85@gmail.com>
2020-08-10 18:56:49 +0545

    Added logout api

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,6 +8,11 @@ module.exports = (app) => {
 
   app.get("/auth/google/callback", passport.authenticate("google"));
 
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
   // shows a user using cookies
   app.get("/api/current_user", (req, res) => {
     res.send(req.user);
