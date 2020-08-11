02c09571c615f0a8126e0395dece02e9801ade06
Sam <samshrestha85@gmail.com>
2020-08-11 15:17:17 +0545

    Finished auth flow and deploying on heroku

---
+++ config/dev.js
@@ -0,0 +1,11 @@
+// dev.js - don't commit this in git!
+module.exports = {
+  googleClientID:
+    "137191721017-vfi2mgfqmj8259ksk0bgjvhj3ia60bl5.apps.googleusercontent.com",
+  googleClientSecret: "fTfzlmw9g2rKN-BACPuZ2NMs",
+  mongoURI:
+    "mongodb+srv://sam:sam@sam-dev.y7gtd.mongodb.net/<dbname>?retryWrites=true&w=majority",
+  cookieKey: "asdrftgyhchfhgfhgfhgfgfhgfhgfghfhfsershyikuljk",
+  facebookID: "3527109597323830",
+  facebookSecret: "7073815ec363e3597d2322f4c84874e8",
+};
