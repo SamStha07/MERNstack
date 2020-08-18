87eab49e77a0c900c516b1abada0534a2d1b498c
Sam <samshrestha85@gmail.com>
2020-08-18 22:45:58 +0545

    Working with stripe for payments

--- config/dev.js
+++ config/dev.js
@@ -9,4 +9,8 @@ module.exports = {
   cookieKey: "asdrftgyhchfhgfhgfhgfgfhgfhgfghfhfsershyikuljk",
   facebookID: "3527109597323830",
   facebookSecret: "7073815ec363e3597d2322f4c84874e8",
+  stripePublishableKey:
+    "pk_test_51HHWdJJ7pnChVHWcfmOzN3c4dXCNV311F3NownTrG3VK22XKfvQUxCcImfgJihxpWD8GZfcdxxifcYLDKQqIDIFs00E8NKMWJs",
+  stripeSecretKey:
+    "sk_test_51HHWdJJ7pnChVHWcTYhe8bZTkGOakfJbk6dSWwVrBnKZIs3zY0D21sHZZNRvkKvZxi2CsLGln2qE9TAcDVZvlX3M003drHUsLY",
 };
